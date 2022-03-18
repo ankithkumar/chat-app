991d6182da907b46a75153e22227801f426b9f0a
ankithkumar <ankith.kumar@hansel.io>
2022-03-18 22:21:24 +0530

    userlist, duplicateuser api

--- server/src/db_conn/user.js
+++ server/src/db_conn/user.js
@@ -8,16 +8,37 @@ async function addUser(userObj) {
     console.log('added user ', res);
 }
 
+async function isUserNameDuplicate(userObj) {
+    await connection.connect();
+    const client = connection.getMongoClient();
+    const res = await client.db("chatApp").collection("user").find(userObj).toArray();
+    console.log('length ', res);
+    await connection.close();
+    return res.length !== 0;
+}
+
 async function isUserPresent(userObj) {
     await connection.connect();
     const client = connection.getMongoClient();
-    const res = await client.db("chatApp").collection("user").find({email: userObj.email, pwd: userObj.pwd}).toArray();
+    const res = await client.db("chatApp").collection("user").find(userObj).toArray();
     console.log('length ', res.length);
     await connection.close();
     return res.length !== 0;
 }
 
+async function getUserList() {
+    await connection.connect();
+    const client = connection.getMongoClient();
+    const res = await client.db("chatApp").collection("user").find({}).sort({"name":1}).toArray();
+    const userList = [];
+    res.forEach(user => userList.push(user.name));
+    console.log(userList);
+    await connection.close();
+    return userList;
+}
 module.exports = {
     addUser,
-    isUserPresent
+    isUserPresent,
+    isUserNameDuplicate,
+    getUserList
 };
